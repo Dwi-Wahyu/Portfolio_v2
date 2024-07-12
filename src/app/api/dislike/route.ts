3d609830f24312451b69bcd0e1742779e41accd9
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2024-07-12 17:37:28 +0800

    remove sharp

--- src/app/api/dislike/route.ts
+++ src/app/api/dislike/route.ts
@@ -16,33 +16,6 @@ export async function POST(request: NextRequest) {
       },
     });
 
-    // const comments = await prisma.comment.findFirst({
-    //   where: { id },
-    // });
-
-    // const likeSum = comments?.like ?? 0;
-
-    // if (likeSum < 1) {
-    //   await prisma.comment.update({
-    //     where: {
-    //       id,
-    //     },
-    //     data: {
-    //       dislike: { increment: 1 },
-    //     },
-    //   });
-    // } else {
-    //   await prisma.comment.update({
-    //     where: {
-    //       id,
-    //     },
-    //     data: {
-    //       dislike: { increment: 1 },
-    //       like: { decrement: 1 },
-    //     },
-    //   });
-    // }
-
     return NextResponse.json({ message: "Sukses" }, { status: 200 });
   } else {
     const subcomments = await prisma.subComment.update({
@@ -53,33 +26,6 @@ export async function POST(request: NextRequest) {
         dislike: { increment: 1 },
       },
     });
-
-    // const subComment = await prisma.subComment.findFirst({
-    //   where: { id },
-    // });
-
-    // const likeSum = subComment?.like ?? 0;
-
-    // if (likeSum < 1) {
-    //   await prisma.subComment.update({
-    //     where: {
-    //       id,
-    //     },
-    //     data: {
-    //       dislike: { increment: 1 },
-    //     },
-    //   });
-    // } else {
-    //   await prisma.subComment.update({
-    //     where: {
-    //       id,
-    //     },
-    //     data: {
-    //       dislike: { increment: 1 },
-    //       like: { decrement: 1 },
-    //     },
-    //   });
-    // }
 
     return NextResponse.json({ message: "Sukses" }, { status: 200 });
   }

efbf106ed0221b38ce1a2f3d638cd2e7cdc8bb5e
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2024-07-14 02:25:40 +0800

    memperbaiki project banksoal

--- src/app/project/banksoal/page.tsx
+++ src/app/project/banksoal/page.tsx
@@ -1,64 +1,124 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import FKGImg from "../../../../public/image/project-img/bg-login-fkg.jpg";
+// import FKGImg from "../../../../public/image/project-img/bg-login-fkg.jpg";
 
 import { IoChevronBackOutline } from "react-icons/io5";
 import { FaGithub, FaNodeJs } from "react-icons/fa";
 import { SiExpress, SiMysql } from "react-icons/si";
+import { ChevronLeft } from "lucide-react";
+
+import loginImg from "../../../../public/image/project-img/banksoal/login-page.png";
+import dashboardImg from "../../../../public/image/project-img/banksoal/dashboard.png";
+import daftarSoalImg from "../../../../public/image/project-img/banksoal/daftar-soal.png";
+import inputSoalImg from "../../../../public/image/project-img/banksoal/input-soal.png";
 
 export default function BanksoalPage() {
   return (
     <div>
-      <div className="block lg:hidden xl:hidden 2xl:hidden">
-        <div className="h-screen relative flex flex-col">
-          <div className="h-[11rem] z-10 bg-transparent"></div>
-          <div className="w-full z-30 flex-grow p-5 text-white rounded-t-3xl bg-gradient-to-r from-black to-gray-900">
-            <div className="w-full flex text-white justify-between items-center mb-4 mt-1">
-              <Link href="/">
-                <IoChevronBackOutline className="text-xl" />
-              </Link>
-
-              <h1 className="w-full text-center font-bold text-lg">
-                Aplikasi Banksoal
-              </h1>
-
-              <Link href="https://github.com/Dwi-Wahyu/banksoal">
-                <FaGithub className="text-xl" />
-              </Link>
-            </div>
+      <div className="w-full text-white align-items flex-col min-h-screen lg:flex xl:flex 2xl:flex bg-gray-900 ">
+        <div className="w-full flex justify-between px-4 pt-7 py-5">
+          <Link href="/">
+            <ChevronLeft className="text-xl" />
+          </Link>
+          <h1 className="text-xl font-bold">Aplikasi Banksoal</h1>
+          <div></div>
+        </div>
+        <div className="w-full px-4 md:px-44 rounded-xl mt-3 flex flex-col gap-7 items-center pb-7">
+          <div className="w-full rounded-lg px-4 py-3 bg-[#1D232A]">
             <table className="table table-border text-center text-white">
               <thead>
                 <tr>
-                  <th colSpan={2}>Tech Stack</th>
+                  <th colSpan={2} className="text-white">
+                    Tech Stack
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 <tr className="grid grid-cols-2">
-                  <td className="border-r border-gray-800">Engine</td>
+                  <td>Engine</td>
                   <td className="flex justify-center">
                     <FaNodeJs className="text-2xl" />
                   </td>
                 </tr>
                 <tr className="grid grid-cols-2">
-                  <td className="border-r border-gray-800">Framework</td>
+                  <td>Framework</td>
                   <td className="flex justify-center">
                     <SiExpress className="text-2xl" />
                   </td>
                 </tr>
-                <tr className="grid grid-cols-2">
-                  <td className="border-r border-gray-800">Database</td>
+                <tr className="grid grid-cols-2 place-items-center">
+                  <td>Database</td>
                   <td className="flex justify-center">
-                    <SiMysql className="text-2xl" />
+                    <SiMysql className="text-3xl" />
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
 
-          <div className="absolute z-20 top-0 left-0">
-            <Image className=" w-full" src={FKGImg} alt="" />
+          <div className="w-full">
+            <h1>Tampilan Aplikasi :</h1>
           </div>
+          <Link
+            className="relative"
+            href="/image/project-img/banksoal/login-page.png"
+          >
+            <Image src={loginImg} alt="" className="rounded-xl min-w-full" />
+            <div className="bg-gradient-to-t from-black flex flex-col justify-end pb-5 to-transparent absolute bottom-0 left-0 rounded-lg w-full h-full shadow">
+              <h1 className="text-center mt-3 underline underline-offset-4">
+                Login Page
+              </h1>
+            </div>
+          </Link>
+
+          <Link
+            href="/image/project-img/banksoal/dashboard.png"
+            className="relative"
+          >
+            <Image
+              src={dashboardImg}
+              alt=""
+              className="rounded-xl min-w-full"
+            />
+            <div className="bg-gradient-to-t from-black flex flex-col justify-end pb-5 to-transparent absolute bottom-0 left-0 rounded-lg w-full h-full shadow">
+              <h1 className="text-center mt-3 underline underline-offset-4">
+                Dashboard
+              </h1>
+            </div>
+          </Link>
+
+          <Link
+            href="/image/project-img/banksoal/daftar-soal.png"
+            className="relative"
+          >
+            <Image
+              src={daftarSoalImg}
+              alt=""
+              className="rounded-xl min-w-full"
+            />
+            <div className="bg-gradient-to-t from-black flex flex-col justify-end pb-5 to-transparent absolute bottom-0 left-0 rounded-lg w-full h-full shadow">
+              <h1 className="text-center mt-3 underline underline-offset-4">
+                Daftar Soal
+              </h1>
+            </div>
+          </Link>
+
+          <Link
+            href="/image/project-img/banksoal/input-soal.png"
+            className="relative"
+          >
+            <Image
+              src={inputSoalImg}
+              alt=""
+              className="rounded-xl min-w-full"
+            />
+            <div className="bg-gradient-to-t from-black flex flex-col justify-end pb-5 to-transparent absolute bottom-0 left-0 rounded-lg w-full h-full shadow">
+              <h1 className="text-center mt-3 underline underline-offset-4">
+                Input Soal
+              </h1>
+            </div>
+          </Link>
         </div>
       </div>
     </div>

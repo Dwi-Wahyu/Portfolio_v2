e1e2b18c3ea6ae72006dc9849df620b1a2961d97
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-08-05 00:29:02 +0800

    fix study program github url in project section

--- src/app/section/ProjectSection.tsx
+++ src/app/section/ProjectSection.tsx
@@ -27,7 +27,7 @@ export default function ProjectSection() {
       subtitle: "Fakultas Kedokteran Gigi Universitas Hasanuddin",
       technologies: [SiCodeigniter, SiMysql, SiBootstrap],
       webUrl: "https://master.dent.unhas.ac.id/",
-      githubUrl: "",
+      githubUrl: "https://github.com/Dwi-Wahyu/fkg-uh-study-program-site",
     },
     {
       imageSrc: "/logo/alikhwan.png",
@@ -66,7 +66,7 @@ export default function ProjectSection() {
       subtitle: "Fakultas Kedokteran Gigi Universitas Hasanuddin",
       technologies: [SiCodeigniter, SiMysql, SiBootstrap],
       webUrl: "https://maxillofacial.dent.unhas.ac.id/",
-      githubUrl: "",
+      githubUrl: "https://github.com/Dwi-Wahyu/fkg-uh-study-program-site",
     },
     {
       imageSrc: "/logo/unhas.png",
